c13ba6fb4b8ff0e596b33a484ad177cfacd4a819
Mehtab Qureshi <mehtabqureshi@MacBookAir.lan>
2025-07-09 09:07:40 +0530

    prov added

--- Azure_terraform/practice_code/main.tf
+++ Azure_terraform/practice_code/main.tf
@@ -1,5 +1,14 @@
 resource "azurerm_resource_group" "rg-block" {
-    name = "rg-macbook"
-    location = "eastus"
-  
+  name     = "rg-macbook"
+  location = "eastus"
+
+}
+
+resource "azurerm_storage_account" "stg-block" {
+
+  name                     = "stgmacbook"
+  resource_group_name      = azurerm_resource_group.rg-block.name
+  location                 = azurerm_resource_group.rg-block.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
